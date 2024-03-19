18d1ee774ef1f6830d980b99bdd52f25ea6ad4c1
danjoe4 <djbroderick4@gmail.com>
2024-03-18 21:47:26 -0400

    first work prototype

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import DropZone from './lib/DropZone.svelte'
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src=logo.svg class="logo" alt="Logo" />
+    </a>
+
+  </div>
+  <h1>Bioinformatics Conversion Tool</h1>
+
+  <div >
+    <DropZone />
+  </div>
+
+  <p>
+    A simple tool to convert bioinformatics file formats. Please contact
+        <a href="mailto:admin@seqbio.app">admin@seqbio.app</a> to report issues,
+        request features, or if you'd like to contribute.
+  </p>
+
+
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+</style>
